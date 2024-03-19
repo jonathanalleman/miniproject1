49b5822f9aad0dbd6cfa4ed181f53dc42222e0f8
jonathanalleman <jonathanalleman@gmail.com>
2024-03-19 18:14:54 -0500

    Finished project

---
+++ script.js
@@ -0,0 +1,15 @@
+fetch('https://fakestoreapi.com/products')
+.then((data)=>{
+    return data.json();
+}). then((completedata)=>{
+    let data1="";
+    completedata.map((values)=>{
+        data1+=`<div class="card">
+        <h1 class="title">${values.title}</h1>
+        <img src=${values.image} alt="img" class="image">
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+    </div>`;
+    });
+    document.getElementById("cards").innerHTML = data1
+})
